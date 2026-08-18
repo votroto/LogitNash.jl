6f7870ef4e9c5970da99179f38b224ff4cbf672e
votroto <tomas.votroubek@outlook.com>
2026-08-19 01:15:30 +0200

    whoa!

--- src/path.jl
+++ src/path.jl
@@ -3,14 +3,6 @@ using LinearAlgebra: BlasInt
 using LinearAlgebra.BLAS: @blasfunc
 using LinearAlgebra.LAPACK: liblapack
 
-@enum StepStatus begin
-    STATUS_SUCCESS
-    STATUS_MAX_ITERS
-    STATUS_SINGULAR
-    STATUS_LARGE_DISTANCE
-    STATUS_JUMP
-end
-
 function validate_game(utils::NTuple{N,AbstractArray}) where N
     if N <= 1
         throw(ArgumentError("A normal-form game must have at least 2 players; got N = $N."))
@@ -35,6 +27,192 @@ function max_deviation_incentive(ubar::NTuple{N}, pi::NTuple{N}) where N
     maximum(maximum(ubar[p]) - dot(ubar[p], pi[p]) for p in 1:N)
 end
 
+function fast_lu!(A::Matrix{Float64}, ipiv::Vector{BlasInt})
+    A, ipiv, info = LinearAlgebra.LAPACK.getrf!(A, ipiv)
+
+    if info > 0
+        # TODO: Don't throw, gracefully return last solution
+        throw(SingularException(info))
+    end
+    return A
+end
+
+function predict!(
+    dx_out::Vector{Float64},
+    x::Vector{Float64},
+    t::Float64,
+    lastdx::Vector{Float64},
+    lastdt::Float64,
+    utils::NTuple{N},
+    ws
+) where {N}
+    mu = splitviews(x, size(first(utils)) .- 1)
+    redlograt_to_prob!.(ws.pi, mu)
+
+    unilateral_derivatives!(ws.dudpi, utils, ws.pi)
+    unilateral_deviations_from_derivatives!(ws.ubar, ws.dudpi, ws.pi)
+
+    jacobian_x!(ws.Fx, ws.pi, t, ws.dudpi, utils)
+    jacobian_t!(ws.Ft, ws.ubar, mu, utils)
+
+    n = length(x)
+    @inbounds for i in 1:n
+        ws.J_aug[end, i] = lastdx[i]
+    end
+    ws.J_aug[end, end] = lastdt
+
+    fill!(ws.rhs_aug, 0.0)
+    ws.rhs_aug[end] = 1.0
+println(cond(ws.J_aug))
+    fast_lu!(ws.J_aug, ws.ipiv)
+
+    cur_det_sign = 1.0
+    n_aug = n + 1
+    @inbounds for i in 1:n_aug
+        if ws.J_aug[i, i] < 0.0
+            cur_det_sign = -cur_det_sign
+        end
+        if ws.ipiv[i] != i
+            cur_det_sign = -cur_det_sign
+        end
+    end
+    if ws.det_sign[1] == 0.0
+        ws.det_sign[1] = cur_det_sign
+    end
+
+    LinearAlgebra.LAPACK.getrs!('N', ws.J_aug, ws.ipiv, ws.rhs_aug)
+
+    norm_factor = 1.0 / sqrt(dot(ws.rhs_aug, ws.rhs_aug))
+
+    @inbounds for i in 1:n
+        dx_out[i] = ws.rhs_aug[i] * norm_factor
+    end
+    dtds = ws.rhs_aug[end] * norm_factor
+
+    return dx_out, dtds
+end
+
+function correct!(
+    xlast::Vector{Float64},
+    tlast::Float64,
+    dx::Vector{Float64},
+    dt::Float64,
+    ds::Float64,
+    utils::NTuple{N},
+    ws;
+    iters::Int=3,
+    abs_tol::Float64=1e-6,
+    rel_tol::Float64=1e-12,
+    bifurcation_ds_tol::Float64=1e-5
+) where {N}
+    @. ws.xpred = xlast + ds * dx
+    tpred = tlast + ds * dt
+
+    copyto!(ws.x_nxt, ws.xpred)
+    t_out = tpred
+
+    n = length(dx)
+    i = 0
+
+    # Track sign locally to prevent corrupting workspace on rejected steps
+    local_det_sign = ws.det_sign[1]
+
+    while true
+        mu = splitviews(ws.x_nxt, size(first(utils)) .- 1)
+        redlograt_to_prob!.(ws.pi, mu)
+
+        unilateral_derivatives!(ws.dudpi, utils, ws.pi)
+        unilateral_deviations_from_derivatives!(ws.ubar, ws.dudpi, ws.pi)
+        residual!(ws.res, mu, ws.ubar, ws.x_nxt, t_out, utils)
+
+        @. ws.x_diff = ws.x_nxt - ws.xpred
+        r_con = dot(ws.x_diff, dx) + (t_out - tpred) * dt
+
+        if dot(ws.res, ws.res) + r_con^2 < abs_tol^2
+            # Commit sign change on early exit success
+            ws.det_sign[1] = local_det_sign
+            return true, ws.x_nxt, t_out
+        end
+
+        jacobian_x!(ws.Fx, ws.pi, t_out, ws.dudpi, utils)
+        jacobian_t!(ws.Ft, ws.ubar, mu, utils)
+
+        @inbounds for j in 1:n
+            ws.J_aug[end, j] = dx[j]
+        end
+        ws.J_aug[end, end] = dt
+
+        @inbounds for j in 1:n
+            ws.rhs_aug[j] = -ws.res[j]
+        end
+        ws.rhs_aug[end] = -r_con
+
+        fast_lu!(ws.J_aug, ws.ipiv)
+
+        # Fold Jump vs Bifurcation Detection ---
+        cur_det_sign = 1.0
+        n_aug = n + 1
+        @inbounds for j in 1:n_aug
+            if ws.J_aug[j, j] < 0.0
+                cur_det_sign = -cur_det_sign
+            end
+            if ws.ipiv[j] != j
+                cur_det_sign = -cur_det_sign
+            end
+        end
+
+        # Distinguish between jumping a fold (large ds) and hitting a bifurcation (tiny ds)
+        if local_det_sign != 0.0 && cur_det_sign != local_det_sign
+            if ds > bifurcation_ds_tol
+                # We likely jumped a tight fold. Reject and halve ds.
+                return false, ws.x_nxt, t_out
+            else
+                # ds is tiny; this is a structural bifurcation. Accept the sign flip.
+                local_det_sign = cur_det_sign
+            end
+        end
+
+        LinearAlgebra.LAPACK.getrs!('N', ws.J_aug, ws.ipiv, ws.rhs_aug)
+
+        dt_step = ws.rhs_aug[end]
+        step_norm_sq = dt_step^2
+
+        @inbounds for j in 1:n
+            dx_step_j = ws.rhs_aug[j]
+            ws.dx_step[j] = dx_step_j
+            step_norm_sq += dx_step_j^2
+        end
+
+        step_norm = sqrt(step_norm_sq)
+        val_norm = sqrt(dot(ws.x_nxt, ws.x_nxt) + t_out^2)
+
+        if step_norm < rel_tol * val_norm
+            @. ws.x_nxt += ws.dx_step
+            t_out += dt_step
+
+            dist_sq = (t_out - tpred)^2
+            @inbounds for j in 1:n
+                dist_sq += (ws.x_nxt[j] - ws.xpred[j])^2
+            end
+            if dist_sq > (2.0 * ds)^2
+                return false, ws.x_nxt, t_out
+            end
+
+            # Step successfully converged and validated. Commit the sign lock.
+            ws.det_sign[1] = local_det_sign
+            return true, ws.x_nxt, t_out
+        end
+
+        if i >= iters
+            return false, ws.x_nxt, t_out
+        end
+
+        @. ws.x_nxt += ws.dx_step
+        t_out += dt_step
+        i += 1
+    end
+end
+
 function make_hc_workspace(x_template::Vector{Float64}, dims::NTuple{N}) where {N}
     T = eltype(x_template)
     n = length(x_template)
@@ -44,8 +222,6 @@ function make_hc_workspace(x_template::Vector{Float64}, dims::NTuple{N}) where {
     res = Vector{T}(undef, rsize)
     ubar = ntuple(i -> zeros(T, dims[i]), Val(N))
     dudpi = ntuple(p -> ntuple(q -> zeros(T, dims[p], dims[q]), Val(N)), Val(N))
-    # q=1 transposed for improved memory access pattern
-    #    dudpi = ntuple(p -> ntuple(q -> (q == 1) ? zeros(dims[q], dims[p]) : zeros(dims[p], dims[q]), Val(N)), Val(N))
 
     J_aug = zeros(T, n + 1, n + 1)
     Fx = view(J_aug, 1:n, 1:n)
@@ -64,201 +240,18 @@ function make_hc_workspace(x_template::Vector{Float64}, dims::NTuple{N}) where {
     return (; pi, res, ubar, dudpi, J_aug, Fx, Ft, ipiv, rhs_aug, xpred, x_diff, dx_step, x_nxt, det_sign)
 end
 
-function lu_det_sign(A::Matrix{Float64}, ipiv::Vector{BlasInt})
-    s = 1.0
-    @inbounds for i in axes(A, 1)
-        if A[i, i] < 0.0
-            s = -s
-        end
-        if ipiv[i] != i
-            s = -s
-        end
-    end
-    return s
-end
-
-function fast_lu!(A::Matrix{Float64}, ipiv::Vector{BlasInt})
-    A, ipiv, info = LinearAlgebra.LAPACK.getrf!(A, ipiv)
-    return info
-end
-
-function predict_step!(xpred::Vector{Float64}, x::Vector{Float64}, t::Float64, dx::Vector{Float64}, dt::Float64, ds::Float64)
-    @. xpred = x + ds * dx
-    tpred = t + ds * dt
-    return xpred, tpred
-end
-
-function predict_direction!(dx::Vector{Float64}, dt::Float64, ws)
-    n = length(dx)
-    fill!(ws.rhs_aug, 0.0)
-    ws.rhs_aug[end] = 1.0
-
-    LinearAlgebra.LAPACK.getrs!('N', ws.J_aug, ws.ipiv, ws.rhs_aug)
-
-    norm_factor = 1.0 / sqrt(dot(ws.rhs_aug, ws.rhs_aug))
-    @inbounds for i in 1:n
-        dx[i] = ws.rhs_aug[i] * norm_factor
-    end
-    dt = ws.rhs_aug[end] * norm_factor
-
-    return dx, dt
-end
-
-function predict_init!(x::Vector{Float64}, t::Float64, utils::NTuple{N}, ws) where {N}
-    n = length(x)
-    mu = splitviews(x, size(first(utils)) .- 1)
-    redlograt_to_prob!.(ws.pi, mu)
-
-    unilateral_derivatives!(ws.dudpi, utils, ws.pi)
-    unilateral_deviations_from_derivatives!(ws.ubar, ws.dudpi, ws.pi)
-
-    jacobian_x!(ws.Fx, ws.pi, t, ws.dudpi, utils)
-    jacobian_t!(ws.Ft, ws.ubar, mu, utils)
-
-    @inbounds for i in 1:n
-        ws.J_aug[end, i] = 0.0
-    end
-    ws.J_aug[end, end] = 1.0
-
-    info = fast_lu!(ws.J_aug, ws.ipiv)
-    if info > 0
-        throw(SingularException(info))
-    end
-
-    ws.det_sign[1] = lu_det_sign(ws.J_aug, ws.ipiv)
-
-    nothing
-end
-
-function correct!(
-    x_nxt::Vector{Float64},
-    xpred::Vector{Float64},
-    tpred::Float64,
-    dx::Vector{Float64},
-    dt::Float64,
-    utils::NTuple{N},
-    ws;
-    max_iters::Int=3,
-    abs_tol::Float64=1e-6,
-    rel_tol::Float64=1e-12
-) where {N}
-    copyto!(x_nxt, xpred)
-    t_out = tpred
-    n = length(x_nxt)
-
-    for i in 0:max_iters
-        mu = splitviews(x_nxt, size(first(utils)) .- 1)
-        redlograt_to_prob!.(ws.pi, mu)
-
-        unilateral_derivatives!(ws.dudpi, utils, ws.pi)
-
-        unilateral_deviations_from_derivatives!(ws.ubar, ws.dudpi, ws.pi)
-        residual!(ws.res, mu, ws.ubar, x_nxt, t_out, utils)
-
-        @. ws.x_diff = x_nxt - xpred
-        r_con = dot(ws.x_diff, dx) + (t_out - tpred) * dt
-
-        # Early exit avoids computing derivatives/factorization
-        if dot(ws.res, ws.res) + r_con^2 < abs_tol^2
-            return STATUS_SUCCESS, i, x_nxt, t_out
-        end
-
-        if i == max_iters
-            return STATUS_MAX_ITERS, max_iters, x_nxt, t_out
-        end
-
-
-
-        jacobian_x!(ws.Fx, ws.pi, t_out, ws.dudpi, utils)
-        jacobian_t!(ws.Ft, ws.ubar, mu, utils)
-
-        @inbounds for j in 1:n
-            ws.J_aug[end, j] = dx[j]
-        end
-        ws.J_aug[end, end] = dt
-
-        @inbounds for j in 1:n
-            ws.rhs_aug[j] = -ws.res[j]
-        end
-        ws.rhs_aug[end] = -r_con
-
-        info = fast_lu!(ws.J_aug, ws.ipiv)
-        if info > 0
-            return STATUS_SINGULAR, i, x_nxt, t_out
-        end
-
-        LinearAlgebra.LAPACK.getrs!('N', ws.J_aug, ws.ipiv, ws.rhs_aug)
-
-        dt_step = ws.rhs_aug[end]
-        step_norm_sq = dt_step^2
-
-        @inbounds for j in 1:n
-            ws.dx_step[j] = ws.rhs_aug[j]
-            step_norm_sq += ws.dx_step[j]^2
-        end
-
-        step_norm = sqrt(step_norm_sq)
-        val_norm = sqrt(dot(x_nxt, x_nxt) + t_out^2)
-
-        @. x_nxt += ws.dx_step
-        t_out += dt_step
-
-        if step_norm < rel_tol * val_norm
-            return STATUS_SUCCESS, i + 1, x_nxt, t_out
-        end
-    end
-
-    return STATUS_MAX_ITERS, max_iters, x_nxt, t_out
-end
-
-function validate_step!(
-    status::StepStatus,
-    iters::Int,
-    x_nxt::Vector{Float64},
-    t_new::Float64,
-    xpred::Vector{Float64},
-    tpred::Float64,
-    ds::Float64,
-    ws
-)
-    if status != STATUS_SUCCESS
-        return status
-    end
-
-    if iters == 0
-        return STATUS_SUCCESS
-    end
-
-    dist_sq = (t_new - tpred)^2
-    @inbounds for j in 1:length(x_nxt)
-        dist_sq += (x_nxt[j] - xpred[j])^2
-    end
-
-    if dist_sq > (2.0 * ds)^2
-        return STATUS_LARGE_DISTANCE
-    end
-
-    cur_det_sign = lu_det_sign(ws.J_aug, ws.ipiv)
-    if ws.det_sign[1] != 0.0 && cur_det_sign != ws.det_sign[1]
-        if dist_sq > (0.5 * ds)^2
-            return STATUS_JUMP
-        else
-            ws.det_sign[1] = cur_det_sign
-        end
-    end
-
-    return STATUS_SUCCESS
-end
-
+using Printf
+function strat_format(xs) join([@sprintf("%.6f ", w) for w in xs]) end
 """
     nash(utils::NTuple{N,AbstractArray{Float64,N}}; stop_iters::Int=1000, stop_t::Float64=1e6, stop_eps::Float64=1e-6) where {N}
+
 Compute an epsilon Nash equilibrium of an N-player game by tracing a logit equilibrium path from t=0 to infinity.
 """
 function nash(
     utils::NTuple{N,AbstractArray{Float64,N}};
     stop_iters::Int=1000,
-    stop_t::Float64=1e6, # TODO: rename to precision
-    stop_eps::Float64=1e-6  # TODO: rename to regret
+    stop_t::Float64=1e6,
+    stop_eps::Float64=1e-6
 ) where {N}
     validate_game(utils)
 
@@ -267,48 +260,50 @@ function nash(
     dx = zero(x)
     dt = 1.0
     ds = 0.01
-
-    ws = make_hc_workspace(x, size(first(utils)))
-    predict_init!(x, t, utils, ws)
-
     iteration = 0
     successes_in_row = 0
     regret = NaN
     stall = false
 
-    while t <= stop_t && iteration <= stop_iters && !stall
-        dx, dt = predict_direction!(dx, dt, ws)
+    ws = make_hc_workspace(x, size(first(utils)))
 
+    while t <= stop_t && iteration <= stop_iters && !stall && t >= -10
+        # @info (@sprintf "%d: slowing at t=%.3f (dt=%.3f, ds=%.3f)" iteration t dt ds)
+
+        dx, dt = predict!(dx, x, t, dx, dt, utils, ws)
         regret = max_deviation_incentive(ws.ubar, ws.pi)
-        if regret <= stop_eps
+        println(strat_format.(ws.pi)..., "$(ws.det_sign[1]) $(round(t;digits=3)) $(round(regret;digits=5)) " )
+
+        if (regret <= stop_eps)
             break
         end
 
-        while true
-            xpred, tpred = predict_step!(ws.xpred, x, t, dx, dt, ds)
-            corr_status, iters, x_nxt, t_nxt = correct!(ws.x_nxt, xpred, tpred, dx, dt, utils, ws)
-            val_status = validate_step!(corr_status, iters, x_nxt, t_nxt, xpred, tpred, ds, ws)
+        corrected = false
+        t_new = t
+        while !corrected
+            corrected, x_new, t_new = correct!(x, t, dx, dt, ds, utils, ws)
 
-            if val_status == STATUS_SUCCESS
-                copyto!(x, x_nxt)
-                t = t_nxt
-                break
-            else
-                ds /= 2.0
+            if !corrected
+                ds /= 2
                 successes_in_row = 0
                 if ds <= 1e-7
+                    # Progress along path stalled!
                     stall = true
                     break
                 end
+            else
+                copyto!(x, x_new)
+                t = t_new
+
+                successes_in_row += 1
+                if successes_in_row >= 5
+                    successes_in_row = 0
+                    ds *= 2
+                end
+
+                iteration += 1
             end
         end
-
-        successes_in_row += 1
-        if successes_in_row >= 5
-            successes_in_row = 0
-            ds *= 2.0
-        end
-        iteration += 1
     end
 
     return ws.pi, (; t, iteration, regret, stall)
